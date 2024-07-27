c9cfdb27a7908a08fd06b422575d671e7ed86c17
Udit <unamdev0@gmail.com>
2024-07-27 13:43:01 +0530

    modified code to take multiple reminders at once, triggering them asynchronously

---
+++ README.md
@@ -0,0 +1,11 @@
+# REMINDER CLI TOOL
+
+A simple reminder cli tool written in go.<br/>
+Accepts new reminder in format `hh:mm text`<br/>
+To start program enter run command<br/>
+`go run main.go`
+
+Tool can take multiple reminders as input as long as the program is running
+
+![Running the tool](<Screenshot 2024-07-27 at 1.39.36 PM.png>)
+![output](<Screenshot 2024-07-27 at 1.39.01 PM.png>)
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
 
@@ -13,16 +13,12 @@ import (
 	"github.com/olebedev/when/rules/en"
 )
 
-const (
-	markName  = "REMINDER_TOOL"
-	markValue = "1"
-)
+func reminder(input string) {
+	inputArray := strings.Split(input, " ")
 
-func main() {
-
-	if len(os.Args) < 3 {
-		fmt.Printf("USAGE: <hh:mm> <text message>\n")
-		os.Exit(1)
+	if len(inputArray) < 2 {
+		fmt.Printf("Please enter in following format <hh:mm> <text message> for this reminder->%s\n", input)
+		return
 	}
 
 	now := time.Now()
@@ -31,38 +27,50 @@ func main() {
 	w.Add(en.All...)
 	w.Add(common.All...)
 
-	r, err := w.Parse(os.Args[1], time.Now())
+	r, err := w.Parse(inputArray[0], time.Now())
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error while reading time for this reminder->%s\n", input)
+		return
 	}
 	if r == nil {
-		fmt.Println(fmt.Errorf("couldn't read the text provided"))
-		os.Exit(2)
+		fmt.Printf("couldn't read the text provided for this reminder->%s\n", input)
+		return
 	}
 	if now.After(r.Time) {
-		fmt.Println(fmt.Errorf("please set future time"))
-		os.Exit(3)
+		fmt.Printf("Time should be of future for this reminder->%s\n", input)
+		return
 	}
 
 	diff := r.Time.Sub(now)
 
-	if os.Getenv(markName) == markValue {
-		time.Sleep(diff)
-		err := beeep.Alert("Reminder", strings.Join(os.Args[2:], " "), "assets/information")
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(4)
-		}
-	} else {
-		cmd := exec.Command(os.Args[0], os.Args[1:]...)
-		cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", markName, markValue))
-		if err := cmd.Start(); err != nil {
-			fmt.Println(err)
-			os.Exit(5)
-		}
+	fmt.Println("Reminder will be run in ", diff.Round(time.Second))
+	time.Sleep(diff)
 
-		fmt.Println("Reminder will be run in ", diff.Round(time.Second))
-		os.Exit(0)
+	err = beeep.Alert("Reminder", strings.Join(inputArray[1:], " "), "assets/information")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func main() {
+
+	var takeInput = true
+
+	fmt.Println("*********Welcome to GO REMINDER TOOL**********")
+	fmt.Println("USAGE: Please enter your time and note to add a reminder in following format\n <hh:mm> <text message>\n For exiting the program please enter 'exit'")
+
+inputLoop:
+	for takeInput {
+		var input string
+		reader := bufio.NewReader(os.Stdin)
+		input, _ = reader.ReadString('\n')
+		// have to use trimspace since we reading till \n
+		if strings.TrimSpace(input) == "exit" {
+			takeInput = false
+			break inputLoop
+		}
+		go reminder(input)
 	}
 
 }
